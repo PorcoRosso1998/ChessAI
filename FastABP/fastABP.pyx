e711006b5dc84ba75e51e201c5822f5eada390e6
Chase Zimmerman <cpzimmer@usc.edu>
2020-11-09 16:53:31 -0800

    Add some cython code, update readme, add gitignore for binaries

---
+++ FastABP/fastABP.pyx
@@ -0,0 +1,80 @@
+import chess
+
+cpdef minimaxRoot(depth, board, isMaximizing):
+    possibleMoves = board.legal_moves
+    cdef int bestMove, value
+    bestMove = -9999
+    bestMoveFinal = None
+    for x in possibleMoves:
+        move = chess.Move.from_uci(str(x))
+        board.push(move)
+        value = max(bestMove, minimax(depth - 1, board,-10000,10000, not isMaximizing))
+        board.pop()
+        if(value > bestMove):
+            print("Best score: ", str(bestMove))
+            print("Best move: ", str(bestMoveFinal))
+            bestMove = value
+            bestMoveFinal = move
+    return bestMoveFinal
+
+cdef minimax(depth, board, alpha, beta, is_maximizing):
+    cdef int bestMove
+    if(depth == 0):
+        return -evaluation(board)
+    possibleMoves = board.legal_moves
+    if(is_maximizing):
+        bestMove = -9999
+        for x in possibleMoves:
+            move = chess.Move.from_uci(str(x))
+            board.push(move)
+            bestMove = max(bestMove,minimax(depth - 1, board,alpha,beta, not is_maximizing))
+            board.pop()
+            alpha = max(alpha,bestMove)
+            if beta <= alpha:
+                return bestMove
+        return bestMove
+    else:
+        bestMove = 9999
+        for x in possibleMoves:
+            move = chess.Move.from_uci(str(x))
+            board.push(move)
+            bestMove = min(bestMove, minimax(depth - 1, board,alpha,beta, not is_maximizing))
+            board.pop()
+            beta = min(beta,bestMove)
+            if(beta <= alpha):
+                return bestMove
+        return bestMove
+
+cdef int evaluation(board):
+    cdef int i, evaluation
+    i = 0
+    evaluation = 0
+    x = True
+    try:
+        x = bool(board.piece_at(i).color)
+    except AttributeError as e:
+        x = x
+    while i < 63:
+        i += 1
+        evaluation = evaluation + (getPieceValue(str(board.piece_at(i))) if x else -getPieceValue(str(board.piece_at(i))))
+    return evaluation
+
+cdef int getPieceValue(piece):
+    if(piece == None):
+        return 0
+    cdef int value 
+    value = 0
+    if piece == "P" or piece == "p":
+        value = 10
+    if piece == "N" or piece == "n":
+        value = 30
+    if piece == "B" or piece == "b":
+        value = 30
+    if piece == "R" or piece == "r":
+        value = 50
+    if piece == "Q" or piece == "q":
+        value = 90
+    if piece == 'K' or piece == 'k':
+        value = 900
+    #value = value if (board.piece_at(place)).color else -value
+    return value
---
+++ README.md
@@ -0,0 +1,15 @@
+# Chess AI
+
+Look into this github repo https://github.com/niklasf/python-chess
+
+## TODO:
+  * Look into using other chess API's with an AI built in to be more efficient. This may be a better idea than using the other Chess API without an AI.
+  * Look into Score class in Python, to determine whether or not it's scoring is good for the application in Min Max.
+  * Look into wdl,
+  
+  
+Here is an example chess AI repo
+  * ```https://github.com/AnthonyASanchez/PythonChessAi ```
+  
+Here is another chess repo
+  * ```https://github.com/thomasahle/sunfish```
